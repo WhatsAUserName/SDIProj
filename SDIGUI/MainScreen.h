2ba632a2e92b3b6c7771ff7501a2d701722a1d01
SMinton <samvinzel@gmail.com>
2018-03-29 12:29:44 +0100

    added basic admin check

--- SDIGUI/MainScreen.h
+++ SDIGUI/MainScreen.h
@@ -74,84 +74,93 @@ namespace SDIGUI {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(13, 13);
+			this->label1->Location = System::Drawing::Point(10, 11);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(313, 36);
+			this->label1->Size = System::Drawing::Size(148, 29);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"DVD App (Name TBD)";
+			this->label1->Text = L"Project Zeus";
+			this->label1->Click += gcnew System::EventHandler(this, &MainScreen::label1_Click);
 			// 
 			// searchBtn
 			// 
-			this->searchBtn->Location = System::Drawing::Point(798, 13);
+			this->searchBtn->Location = System::Drawing::Point(598, 11);
+			this->searchBtn->Margin = System::Windows::Forms::Padding(2);
 			this->searchBtn->Name = L"searchBtn";
-			this->searchBtn->Size = System::Drawing::Size(115, 36);
+			this->searchBtn->Size = System::Drawing::Size(86, 29);
 			this->searchBtn->TabIndex = 1;
 			this->searchBtn->Text = L"Search";
 			this->searchBtn->UseVisualStyleBackColor = true;
+			this->searchBtn->Click += gcnew System::EventHandler(this, &MainScreen::searchBtn_Click);
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(19, 101);
+			this->label2->Location = System::Drawing::Point(14, 82);
+			this->label2->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(68, 17);
+			this->label2->Size = System::Drawing::Size(89, 13);
 			this->label2->TabIndex = 2;
-			this->label2->Text = L"Released";
+			this->label2->Text = L"Recent Releases";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(335, 101);
+			this->label3->Location = System::Drawing::Point(251, 82);
+			this->label3->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(81, 17);
+			this->label3->Size = System::Drawing::Size(55, 13);
 			this->label3->TabIndex = 3;
-			this->label3->Text = L"Unreleased";
+			this->label3->Text = L"Upcoming";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(693, 101);
+			this->label4->Location = System::Drawing::Point(520, 82);
+			this->label4->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(85, 17);
+			this->label4->Size = System::Drawing::Size(66, 13);
 			this->label4->TabIndex = 4;
 			this->label4->Text = L"Now Playing";
 			// 
 			// listBox1
 			// 
 			this->listBox1->FormattingEnabled = true;
-			this->listBox1->ItemHeight = 16;
 			this->listBox1->Items->AddRange(gcnew cli::array< System::Object^  >(1) { L"Ready Player One" });
-			this->listBox1->Location = System::Drawing::Point(22, 146);
+			this->listBox1->Location = System::Drawing::Point(16, 119);
+			this->listBox1->Margin = System::Windows::Forms::Padding(2);
 			this->listBox1->Name = L"listBox1";
-			this->listBox1->Size = System::Drawing::Size(217, 180);
+			this->listBox1->Size = System::Drawing::Size(164, 147);
 			this->listBox1->TabIndex = 5;
+			this->listBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MainScreen::listBox1_SelectedIndexChanged);
 			// 
 			// listBox2
 			// 
 			this->listBox2->FormattingEnabled = true;
-			this->listBox2->ItemHeight = 16;
 			this->listBox2->Items->AddRange(gcnew cli::array< System::Object^  >(1) { L"My Secret Screenplay" });
-			this->listBox2->Location = System::Drawing::Point(338, 146);
+			this->listBox2->Location = System::Drawing::Point(254, 119);
+			this->listBox2->Margin = System::Windows::Forms::Padding(2);
 			this->listBox2->Name = L"listBox2";
-			this->listBox2->Size = System::Drawing::Size(217, 180);
+			this->listBox2->Size = System::Drawing::Size(164, 147);
 			this->listBox2->TabIndex = 6;
 			this->listBox2->SelectedIndexChanged += gcnew System::EventHandler(this, &MainScreen::listBox2_SelectedIndexChanged);
 			// 
 			// listBox3
 			// 
 			this->listBox3->FormattingEnabled = true;
-			this->listBox3->ItemHeight = 16;
 			this->listBox3->Items->AddRange(gcnew cli::array< System::Object^  >(1) { L"Ready Player One" });
-			this->listBox3->Location = System::Drawing::Point(696, 146);
+			this->listBox3->Location = System::Drawing::Point(522, 119);
+			this->listBox3->Margin = System::Windows::Forms::Padding(2);
 			this->listBox3->Name = L"listBox3";
-			this->listBox3->Size = System::Drawing::Size(217, 180);
+			this->listBox3->Size = System::Drawing::Size(164, 147);
 			this->listBox3->TabIndex = 7;
 			// 
 			// MainScreen
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(925, 463);
+			this->BackColor = System::Drawing::SystemColors::GradientActiveCaption;
+			this->ClientSize = System::Drawing::Size(694, 376);
 			this->Controls->Add(this->listBox3);
 			this->Controls->Add(this->listBox2);
 			this->Controls->Add(this->listBox1);
@@ -160,6 +169,7 @@ namespace SDIGUI {
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->searchBtn);
 			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"MainScreen";
 			this->Text = L"MainScreen";
 			this->ResumeLayout(false);
@@ -169,5 +179,13 @@ namespace SDIGUI {
 #pragma endregion
 	private: System::Void listBox2_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
 	}
+private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+}
+private: System::Void searchBtn_Click(System::Object^  sender, System::EventArgs^  e) {
+	//open search pane
+}
+private: System::Void listBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+	//open relevant movie page
+}
 };
 }
--- SDIGUI/MyForm.h
+++ SDIGUI/MyForm.h
@@ -17,6 +17,7 @@ namespace SDIGUI {
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
+		bool admin = false;
 		MyForm(void)
 		{
 			InitializeComponent();
@@ -24,6 +25,7 @@ namespace SDIGUI {
 			//TODO: Add the constructor code here
 			//
 		}
+		
 
 	protected:
 		/// <summary>
@@ -41,7 +43,7 @@ namespace SDIGUI {
 	private: System::Windows::Forms::TextBox^  textBox2;
 	private: System::Windows::Forms::Label^  label1;
 	private: System::Windows::Forms::Label^  label2;
-	private: System::Windows::Forms::Button^  button1;
+
 	protected:
 
 	protected:
@@ -65,14 +67,14 @@ namespace SDIGUI {
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// logOnBtn
 			// 
-			this->logOnBtn->Location = System::Drawing::Point(101, 181);
+			this->logOnBtn->Location = System::Drawing::Point(76, 147);
+			this->logOnBtn->Margin = System::Windows::Forms::Padding(2);
 			this->logOnBtn->Name = L"logOnBtn";
-			this->logOnBtn->Size = System::Drawing::Size(75, 25);
+			this->logOnBtn->Size = System::Drawing::Size(56, 23);
 			this->logOnBtn->TabIndex = 0;
 			this->logOnBtn->Text = L"Log In";
 			this->logOnBtn->UseVisualStyleBackColor = true;
@@ -80,24 +82,29 @@ namespace SDIGUI {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(66, 129);
+			this->textBox1->Location = System::Drawing::Point(50, 105);
+			this->textBox1->Margin = System::Windows::Forms::Padding(2);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(151, 22);
+			this->textBox1->Size = System::Drawing::Size(114, 20);
 			this->textBox1->TabIndex = 1;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(66, 60);
+			this->textBox2->Location = System::Drawing::Point(50, 49);
+			this->textBox2->Margin = System::Windows::Forms::Padding(2);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(151, 22);
+			this->textBox2->Size = System::Drawing::Size(114, 20);
 			this->textBox2->TabIndex = 2;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(66, 22);
+			this->label1->Location = System::Drawing::Point(50, 18);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(79, 17);
+			this->label1->Size = System::Drawing::Size(60, 13);
 			this->label1->TabIndex = 3;
 			this->label1->Text = L"User Name";
 			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
@@ -105,32 +112,25 @@ namespace SDIGUI {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(66, 100);
+			this->label2->Location = System::Drawing::Point(50, 81);
+			this->label2->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(69, 17);
+			this->label2->Size = System::Drawing::Size(53, 13);
 			this->label2->TabIndex = 4;
 			this->label2->Text = L"Password";
 			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(78, 230);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(125, 25);
-			this->button1->TabIndex = 5;
-			this->button1->Text = L"Log in as Admin";
-			this->button1->UseVisualStyleBackColor = true;
-			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(280, 289);
-			this->Controls->Add(this->button1);
+			this->BackColor = System::Drawing::SystemColors::GradientActiveCaption;
+			this->ClientSize = System::Drawing::Size(210, 200);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->logOnBtn);
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
@@ -139,11 +139,21 @@ namespace SDIGUI {
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		String^ username = textBox2->Text;
+		String^ password = textBox1->Text;
+		if (username == "admin" && password == "password")
+		{
+			admin = true;
+		}
 		MyForm(Close);
 		MainScreen^ fs = gcnew MainScreen();
 		fs->ShowDialog();
 	}
 	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
 	}
+private: System::Void textBox2_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+}
+private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+}
 };
 }
